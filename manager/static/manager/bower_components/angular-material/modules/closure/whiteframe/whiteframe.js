d439d5c01c6e412b978fb38510f9ec08304aa2e6
xlorepdarkhelm <xlorep@darkhelm.org>
2015-11-09 18:31:28 +0000

    Working on getting Angular Material for the basis of the client-side components of the system.

---
+++ manager/static/manager/bower_components/angular-material/modules/closure/whiteframe/whiteframe.js
@@ -0,0 +1,15 @@
+/*!
+ * Angular Material Design
+ * https://github.com/angular/material
+ * @license MIT
+ * v1.0.0-rc3-master-1ae16cb
+ */
+goog.provide('ng.material.components.whiteframe');
+
+/**
+ * @ngdoc module
+ * @name material.components.whiteframe
+ */
+angular.module('material.components.whiteframe', []);
+
+ng.material.components.whiteframe = angular.module("material.components.whiteframe");

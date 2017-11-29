3b301cfa4ca5593caaf8a90b6dbbe6cae5ce0367
Cliff Hill <github@darkhelm.org>
2017-11-29 13:46:50 -0500

    Adding two missed files.

---
+++ playlist/core/sql/__init__.py
@@ -0,0 +1,12 @@
+"""
+The pampas.sql package contains all SQL database-specific code.
+
+It is very reliant on the SQLAlchemy plugin for python to do all SQL
+operations.
+"""
+
+__all__ = (
+    'conn',
+    'tables',
+    'sqllib',
+)
